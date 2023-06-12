ed7faed3a036a700598ed1f66536892b968e1705
Hashir-Khan777 <khnhashir555@gmail.com>
2023-06-12 14:10:18 +0500

    disable cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors({ origin: process.env.ORIGIN }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
